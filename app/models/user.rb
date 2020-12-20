5656a84452717e090eee04fec733049e3567cdf3
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-12-20 20:41:08 +0900

    feat: パスワード登録の際に制限をかけるため、バリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :email
-    validates :password
+    validates :password, format: { with: /\A(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-z0-9]{8,32})\z/ }
     validates :password_confirmation
   end
 end
