38ddc87f59bca7dd6dfbec85a9e984d89bb11844
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-12-20 20:32:57 +0900

    feat: 何も入力していないと登録できない様に'presenceオプション'を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :name
+    validates :email
+    validates :password
+    validates :password_confirmation
+  end
 end
