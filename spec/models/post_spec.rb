1a4220c7317be34ca30147b5c12775c5f06dcabd
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-12-21 00:07:30 +0900

    test: Postモデルの正常系テストコード実装

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,13 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  let(:content) { build(:content) }
+  let(:post) { build(:post) }
 
   describe '掲示板新規投稿' do
     context '新規投稿ができる場合' do
       it '投稿の内容が入力されている' do
+        expect(post).to be_valid
       end
       it '内容が140文字以内で入力されている' do
+        post.content = Faker::Lorem.characters(number: 140)
+        expect(post).to be_valid
       end
     end
     context '新規投稿ができない場合' do
